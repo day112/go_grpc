e9270ba8cc0cf50c732c52d2600c4cf5c88d2c36
hidebon0630 <hyagi0630@icloud.com>
2021-10-23 17:44:55 +0900

    基板作成

---
+++ article/repository/repository.go
@@ -0,0 +1,65 @@
+package repository
+
+import (
+	"context"
+	"database/sql"
+	"go_grpc/pb"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+type Repository interface {
+	InsertArticle(ctx context.Context, input *pb.ArticleInput) (int64, error)
+	SelectArticleByID(ctx context.Context, id int64) (*pb.Article, error)
+	UpdateArticle(ctx context.Context, id int64, input *pb.ArticleInput) error
+	DeleteArticle(ctx context.Context, id int64) error
+	SelectAllArticles(*sql.Rows, error) error
+}
+
+type sqliteRepo struct {
+	db *sql.DB
+}
+
+func NewsqliteRepo() (Repository, error) {
+	db, err := sql.Open("sqlite3", "./article/article.sql")
+	if err != nil {
+		return nil, err
+	}
+
+	// articleテーブルを作成
+	cmd := `CREATE TABLE IF NOT EXISTS articles(
+		id INTEGER PRIMARY KEY AUTOINCREMENT,
+		author STRING,
+		title STRING,
+		content STRING)`
+
+	_, err = db.Exec(cmd)
+	if err != nil {
+		return nil, err
+	}
+	return &sqliteRepo{db}, nil
+}
+
+// DBに記事をINSERT
+func (r *sqliteRepo) InsertArticle(ctx context.Context, input *pb.ArticleInput) (int64, error) {
+}
+
+// DBからIDに基づいて記事をSELECT
+func (r *sqliteRepo) SelectArticleByID(ctx context.Context, id int64) (*pb.Article, error) {
+
+}
+
+// DB内の記事をUPDATE
+func (r *sqliteRepo) UpdateArticle(ctx context.Context, id int64, input *pb.ArticleInput) error {
+
+}
+
+// DB内の記事をDELETE
+func (r *sqliteRepo) DeleteArticle(ctx context.Context, id int64) error {
+
+}
+
+// articlesテーブルの記事を全取得
+func (r *sqliteRepo) SelectAllArticles() (*sql.Rows, error) {
+
+}
