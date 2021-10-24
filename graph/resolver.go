a851098674412f4dd6487637b3914050e475dd65
hidebon0630 <hyagi0630@icloud.com>
2021-10-24 17:53:17 +0900

    CREATE

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,6 +1,11 @@
 package graph
 
-import "go_grpc/article/client"
+import (
+	"context"
+	"go_grpc/article/client"
+	"go_grpc/article/pb"
+	"go_grpc/graph/model"
+)
 
 // This file will not be regenerated automatically.
 //
@@ -8,4 +13,21 @@ import "go_grpc/article/client"
 
 type Resolver struct {
 	ArticleClient *client.Client
+}
+
+func (r *mutationResolver) CreateArticle(ctx context.Context, input model.CreateInput) (*model.Article, error) {
+	// gRPCサーバーでArticleをCREATE
+	article, err := r.ArticleClient.CreateArticle(
+		ctx,
+		&pb.ArticleInput{
+			Author:  input.Author,
+			Title:   input.Title,
+			Content: input.Content,
+		})
+	if err != nil {
+		return nil, err
+	}
+
+	// CREATEしたArticleを返す
+	return article, nil
 }
