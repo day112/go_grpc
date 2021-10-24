b3e6d74c463e1db0a043d185235828a3c2cd20c2
hidebon0630 <hyagi0630@icloud.com>
2021-10-24 18:20:08 +0900

    playground 起動

--- graph/resolver.go
+++ graph/resolver.go
@@ -1,10 +1,7 @@
 package graph
 
 import (
-	"context"
 	"go_grpc/article/client"
-	"go_grpc/article/pb"
-	"go_grpc/graph/model"
 )
 
 // This file will not be regenerated automatically.
@@ -13,72 +10,4 @@ import (
 
 type Resolver struct {
 	ArticleClient *client.Client
-}
-
-func (r *mutationResolver) CreateArticle(ctx context.Context, input model.CreateInput) (*model.Article, error) {
-	// gRPCサーバーでArticleをCREATE
-	article, err := r.ArticleClient.CreateArticle(
-		ctx,
-		&pb.ArticleInput{
-			Author:  input.Author,
-			Title:   input.Title,
-			Content: input.Content,
-		})
-	if err != nil {
-		return nil, err
-	}
-
-	// CREATEしたArticleを返す
-	return article, nil
-}
-
-func (r *mutationResolver) UpdateArticle(ctx context.Context, input model.UpdateInput) (*model.Article, error) {
-	// gRPCサーバーでArticleをUPDATE
-	article, err := r.ArticleClient.UpdateArticle(
-		ctx,
-		int64(input.ID),
-		&pb.ArticleInput{
-			Author:  input.Author,
-			Title:   input.Title,
-			Content: input.Content,
-		})
-	if err != nil {
-		return nil, err
-	}
-
-	// UPDATEしたArticleを返す
-	return article, nil
-}
-
-func (r *mutationResolver) DeleteArticle(ctx context.Context, input int) (int, error) {
-	// gRPCサーバーでArticleをDELETE
-	id, err := r.ArticleClient.DeleteArticle(ctx, int64(input))
-	if err != nil {
-		return 0, err
-	}
-
-	// DELETEしたArticleのIDを返す
-	return int(id), nil
-}
-
-func (r *queryResolver) Article(ctx context.Context, input int) (*model.Article, error) {
-	// 入力したIDの記事をgRPCサーバーからREAD
-	article, err := r.ArticleClient.ReadArticle(ctx, int64(input))
-	if err != nil {
-		return nil, err
-	}
-
-	// READしたArticleを返す
-	return article, nil
-}
-
-func (r *queryResolver) Articles(ctx context.Context) ([]*model.Article, error) {
-	// gRPCサーバーでArticleを全取得
-	articles, err := r.ArticleClient.ListArticle(ctx)
-	if err != nil {
-		return nil, err
-	}
-
-	// 全取得したArticleを返す
-	return articles, nil
 }
